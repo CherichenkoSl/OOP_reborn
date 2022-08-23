7105fa2f220f86c2e9befe061e31f15ec7fd6699
Слава <@DESKTOP-5KMABHU>
2022-08-23 15:49:20 +0300

    Добавьте файлы проекта.

---
+++ Lesson2/Program.cs
@@ -0,0 +1,12 @@
+﻿public class BankAccount
+{
+    private int _id;
+    private string _balance;
+    private enum _type
+    {
+        settlement,
+        credit,
+        deposit,
+        budgetary
+    }
+}
