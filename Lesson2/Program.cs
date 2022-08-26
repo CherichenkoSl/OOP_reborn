881f562adc1f4be1e7ef1ae564992c96141f6308
Вячеслав Чериченко <cherry-slava2@yandex.ru>
2022-08-27 02:57:40 +0300

    first task completed

--- Lesson2/Program.cs
+++ Lesson2/Program.cs
@@ -1,12 +1,60 @@
-﻿public class BankAccount
+﻿BankAccount bank = new BankAccount();
+bank.SetId(21376231);
+bank.SetBalance(2313.4353m);
+bank.SetType("credit");
+Console.WriteLine($"ID:{bank.SeeId()} Balance:{bank.SeeBalance()} Type:{bank.SeeType()}");
+public class BankAccount
 {
     private int _id;
-    private string _balance;
-    private enum _type
+    public int SeeId()
+    {
+       return _id;
+    }
+    public void SetId(int id)
+    {
+        _id=id;
+    }
+    private decimal _balance;
+    public decimal SeeBalance()
+    {
+        return _balance;
+    }
+    public void SetBalance(decimal balance)
+    {
+        _balance = balance;
+    }
+    private enum Type
     {
         settlement,
         credit,
         deposit,
         budgetary
     }
+    private Type _type;
+    public void SetType(string type)
+    {
+        switch(type)
+        {
+            case "settlement":
+                _type=Type.settlement ;
+                break;
+            case "credit":
+                _type = Type.credit;
+                break;
+            case "deposit":
+                _type = Type.deposit;
+                break;
+            case "budgetary":
+                _type = Type.budgetary;
+                break;
+                default : 
+                Console.WriteLine("Указан неккоректный тип счета");
+                break;
+        }
+    }
+    public string SeeType()
+    {
+        return _type.ToString();
+    }
 }
+
